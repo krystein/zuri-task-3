e33b5b1e7f50d8d2d5daa0a835c9238201807a87
krystein <nworahchimaobic@gmail.com>
2023-09-22 18:56:20 +0100

    task

--- src/components/index.js
+++ src/components/index.js
@@ -14,7 +14,7 @@ const Index=()=> {
   };
   return (
     <div style={{background:
-      "linear-gradient(136.51deg, #4C7FB5 1.15%, rgba(76, 127, 181, 0) 101.51%)", height: "90vh", maxHeight: "2200px"}}>
+      "linear-gradient(136.51deg, #4C7FB5 1.15%, rgba(76, 127, 181, 0) 101.51%)", }}>
       <Navbar toggle={toggle}/>
       <Dropdown isOpen={isOpen} toggle={toggle}/>
       <div className="container p-2">
