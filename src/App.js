21f8d84f749be0c6ce0b3b204af6f9d396d716a0
krystein <nworahchimaobic@gmail.com>
2023-09-22 19:22:08 +0100

    task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import GlobalStyle from "./globalstyles";
 import Index from "./components";
+import Navbar from "./components/navbar";
+import Dropdown from "./components/Dropdown";
 
 
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>
+    <div style={{background:
+      "linear-gradient(136.51deg, #4C7FB5 1.15%, rgba(76, 127, 181, 0) 101.51%)", }}>
       <GlobalStyle/>
+      <Navbar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Index />
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,14 @@
-import React, {useState} from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ImageGallery from "./ImageGallery";
-import Navbar from "./navbar";
-import Dropdown from "./Dropdown";
+
 
 
 
 const Index=()=> {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
+ 
   return (
-    <div style={{background:
-      "linear-gradient(136.51deg, #4C7FB5 1.15%, rgba(76, 127, 181, 0) 101.51%)", }}>
-      <Navbar toggle={toggle}/>
-      <Dropdown isOpen={isOpen} toggle={toggle}/>
+    <div>
       <div className="container p-2">
       <ImageGallery />
       </div>
